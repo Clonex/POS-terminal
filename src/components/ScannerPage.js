9caab08cf3c6029c58ee1751f1dfeec2a80546eb
Michael <clonex_kontakt@hotmail.com>
2020-04-17 14:25:14 +0200

    Fixed keyboard bug

--- src/components/ScannerPage.js
+++ src/components/ScannerPage.js
@@ -123,7 +123,7 @@ export default class ScannerPage extends React.Component {
                 this.state.checkPass ? 
                 <div className="keyboard">
                     {
-                        "1234567890".split("").map(d => <button key={d} onKeyDown={() => requestAnimationFrame(() => doKey(d, this.refs.pass))}>{d}</button>)
+                        "1234567890".split("").map(d => <button key={d} onClick={() => requestAnimationFrame(() => doKey(d, this.refs.pass))}>{d}</button>)
                     }
                     <button className="fourth" onClick={() => doKey(-1, this.refs.pass)}>&#x3c;</button>
                     <button className="fourth" onClick={() => this.simplePassCheck(this.refs.pass)}>Send</button>
